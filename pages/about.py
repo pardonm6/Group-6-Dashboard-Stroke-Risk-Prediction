31d3f4e885101a609855461c82c9abd04ea62316
pamcast <pamela.cast.glez@gmail.com>
2025-09-24 22:32:59 +0200

    I added details to the dashboard
    please pull

---
+++ pages/about.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+from forms.contact import contact_form
+
+#---define forms for contact---
+@st.experimental_dialog("Contact Us")
+def show_contact_form():
+    contact_form()
+
+
+#--Neuro predict Team---
+col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
+with col1: 
+    st.image("./assets/SU_Logo.svg", width=200)
+with col2:
+    st. title("Neuro Predict Team", anchor =False)
+    st.write(" We are a team of healthcare professionals currently studying a Masters in Health Informatics " \
+    "dedicated to improve stroke prediction and patient outcomes. Our mission is to harness the power of data to provide actionable insights "
+    "for healthcare professionals, ultimately enhancing patient care and reducing the incidence of stroke and the related complications.")
+    if st.button('✉️Contact us'):
+        show_contact_form()
