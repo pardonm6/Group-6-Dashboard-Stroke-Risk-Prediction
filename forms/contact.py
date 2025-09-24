c117fd7748e4ea60f3d3087767c15d6232b7f107
pamcast <pamela.cast.glez@gmail.com>
2025-09-24 22:48:54 +0200

    identation problem

--- forms/contact.py
+++ forms/contact.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 def contact_form():
     with st.form("contact_form"):
-    st.text_input("First Name")
-    st.text_input("Last Name")
-    st.text_input("Email")
-    st.text_area("Message")
-    submitted = st.form_submit_button("Submit")
+        st.text_input("First Name")
+        st.text_input("Last Name")
+        st.text_input("Email")
+        st.text_area("Message")
+        submitted = st.form_submit_button("Submit")
     
-    if submitted:
-        st.success("Thank you for reaching out! We will get back to you soon.")
+        if submitted:
+            st.success("Thank you for reaching out! We will get back to you soon.")
