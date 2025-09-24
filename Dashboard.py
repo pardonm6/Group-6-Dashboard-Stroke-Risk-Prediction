646994c996cf43b471cb14b029a90f5a76514469
pamcast <pamela.cast.glez@gmail.com>
2025-09-24 23:05:36 +0200

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -6,33 +6,29 @@ st.set_page_config(
 )
 
 # Sidebar configuration
-st.sidebar.image("./assets/NeuroPredict.png",)
+st.sidebar.image("./assets/project-logo.jpg",)
 st.sidebar.success("Select a tab above.")
 
 # # Page information
 
-st.write("# Welcome to PROHI Dashboard! 👋")
+st.write("# Welcome to Stroke prediction Dashboard ")
 
-st.markdown(
-"""
-    ## Aims
+#--- Page setup---
 
-    After completing the course the student should be able to:
-    - explain basic project management methods
-    - be able to account for success factors in Health Informatics projects
-    - understand basic methods and tools in the field of data science and machine learning
-    - explain process models for data mining projects
-    - explain the difference between rule-based methods and machine learning methods
-    - apply basic project management methods
-    - work in an international multidisciplinary project group
-    - independently lead and implement a limited project in health informatics - document the steps in the design of a prototype for a health informatics project
-    - apply the steps in a process model for data mining projects
-    - apply methods from the field of text mining on different types of health informatics problems
-    - explain and argue for their positions regarding the implementation of a health informatics project
-    - explain how to work with sensitive health information in a safe and ethical way.
-    
-"""
+about_page = st.Page(page= "pages/About.py",title= "About", icon= ":material/info:",default=True,)
+project_1_page = st.Page(page= "pages/Descriptive_Analytics", title ="Descriptive Analytics", icon = "material/bar_chart:")
+project_2_page = st.Page(page= "pages/Predictive_Analytics.py", title= "Predictive Analytics", icon = "material/batch_prediction:")
+
+#--Navigation setup [sections]--
+pg = st.navigation(
+    {
+        "Info": [about_page],
+        "Project": [project_1_page, project_2_page],
+    }
 )
+#---Run navigation---
+pg.run()
+
 
 # You can also add text right into the web as long comments (""")
 """
