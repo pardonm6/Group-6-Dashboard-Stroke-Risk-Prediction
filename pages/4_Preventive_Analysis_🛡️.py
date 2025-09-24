5a9b5f2bf1e89418644084fccf2c77f327f71233
htetwaiaung-hwa <drhtetwaiaung@gmail.com>
2025-09-24 20:23:24 +0200

    Update_Pages
    24-Sep-2025,18:22 - Updated names on Diagnostic, Preventive and About pages

---
+++ pages/4_Preventive_Analysis_🛡️.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.markdown("# Preventive Analytics 🛡️")
+
+st.sidebar.markdown("# Peventive Analytics 🛡️")
---
+++ pages/5_About_ℹ️.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.markdown("# About ℹ️")
+
+st.sidebar.markdown("# About ℹ️")
